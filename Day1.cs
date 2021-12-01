6148d0e6206767d457e7ac79bdebf8de4c366b79
mike <mike@MIKE10>
2021-11-30 23:02:54 -0800

    Add project files.

---
+++ Day1.cs
@@ -0,0 +1,68 @@
+﻿using Advent2020;
+using NUnit.Framework;
+
+namespace Advent2021
+{
+    [TestFixture]
+    public class Day1
+    {
+        const string _inputFile = @"Inputs\Day1.txt";
+        const string _testData = @"199
+200
+208
+210
+200
+207
+240
+269
+260
+263";
+
+        [Test]
+        public void SilverTest()
+        {
+            var inputs = FileHelpers.ReadAllLinesFromString(_testData).Select(i => Convert.ToInt64(i)).ToArray();
+
+            Assert.AreEqual(7, Run(1, inputs));
+        }
+
+        [Test]
+        public void Silver()
+        {
+            var inputs = FileHelpers.EnumerateLines(_inputFile).Select(i => Convert.ToInt64(i)).ToArray();
+
+            Assert.AreEqual(1655, Run(1, inputs));
+        }
+
+        [Test]
+        public void GoldTest()
+        {
+            var inputs = FileHelpers.ReadAllLinesFromString(_testData).Select(i => Convert.ToInt64(i)).ToArray();
+
+            Assert.AreEqual(5, Run(3, inputs));
+        }
+
+        [Test]
+        public void Gold()
+        {
+            var inputs = FileHelpers.EnumerateLines(_inputFile).Select(i => Convert.ToInt64(i)).ToArray();
+
+            Assert.AreEqual(1683, Run(3, inputs));
+        }
+
+        static int Run(int windowSize, IList<long> inputs)
+        {
+            int total = 0;
+
+            for (int index = windowSize; index < inputs.Count; index++)
+            {
+                if (inputs[index] > inputs[index - windowSize])
+                {
+                    total++;
+                }
+            }
+
+            return total;
+        }
+    }
+}
---
+++ FileHelpers.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+
+namespace Advent2020
+{
+	static class FileHelpers
+	{
+		public static IEnumerable<string> EnumerateLines(string filename)
+		{
+			using var inputFile = new StreamReader(File.OpenRead(filename));
+			while (!inputFile.EndOfStream)
+			{
+				var line = inputFile.ReadLine();
+				if (line == null) break;
+				yield return line;
+			}
+		}
+
+		public static string GetSingle(string filename)
+		{
+			using var inputFile = new StreamReader(File.OpenRead(filename));
+			var line = inputFile.ReadLine();
+			if (line == null) return string.Empty;
+			return line;
+		}
+
+		public static IEnumerable<string> ReadAllLinesFromString(string input)
+		{
+			using var inputStream = new StringReader(input);
+
+			for (; ; )
+			{
+				var line = inputStream.ReadLine();
+				if (line == null) yield break;
+
+				yield return line;
+			}
+		}
+
+		public static void CheckInputs(string filename)
+		{
+			if (!File.Exists(filename))
+			{
+				Assert.Inconclusive("Inputs not available");
+			}
+		}
+	}
+}
