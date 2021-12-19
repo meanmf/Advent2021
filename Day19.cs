0e4def3d75625212a371308446216c2e6cb93cb3
meanmf <14282797+meanmf@users.noreply.github.com>
2021-12-19 02:23:48 -0800

    Day 19 hax

---
+++ Day19.cs
@@ -0,0 +1,340 @@
+﻿using Advent2020;
+using NUnit.Framework;
+
+namespace Advent2021
+{
+    [TestFixture]
+    internal class Day19
+    {
+        const string _inputFilename = @"Inputs\Day19.txt";
+        #region test data
+
+        const string _testData = @"--- scanner 0 ---
+404,-588,-901
+528,-643,409
+-838,591,734
+390,-675,-793
+-537,-823,-458
+-485,-357,347
+-345,-311,381
+-661,-816,-575
+-876,649,763
+-618,-824,-621
+553,345,-567
+474,580,667
+-447,-329,318
+-584,868,-557
+544,-627,-890
+564,392,-477
+455,729,728
+-892,524,684
+-689,845,-530
+423,-701,434
+7,-33,-71
+630,319,-379
+443,580,662
+-789,900,-551
+459,-707,401
+
+--- scanner 1 ---
+686,422,578
+605,423,415
+515,917,-361
+-336,658,858
+95,138,22
+-476,619,847
+-340,-569,-846
+567,-361,727
+-460,603,-452
+669,-402,600
+729,430,532
+-500,-761,534
+-322,571,750
+-466,-666,-811
+-429,-592,574
+-355,545,-477
+703,-491,-529
+-328,-685,520
+413,935,-424
+-391,539,-444
+586,-435,557
+-364,-763,-893
+807,-499,-711
+755,-354,-619
+553,889,-390
+
+--- scanner 2 ---
+649,640,665
+682,-795,504
+-784,533,-524
+-644,584,-595
+-588,-843,648
+-30,6,44
+-674,560,763
+500,723,-460
+609,671,-379
+-555,-800,653
+-675,-892,-343
+697,-426,-610
+578,704,681
+493,664,-388
+-671,-858,530
+-667,343,800
+571,-461,-707
+-138,-166,112
+-889,563,-600
+646,-828,498
+640,759,510
+-630,509,768
+-681,-892,-333
+673,-379,-804
+-742,-814,-386
+577,-820,562
+
+--- scanner 3 ---
+-589,542,597
+605,-692,669
+-500,565,-823
+-660,373,557
+-458,-679,-417
+-488,449,543
+-626,468,-788
+338,-750,-386
+528,-832,-391
+562,-778,733
+-938,-730,414
+543,643,-506
+-524,371,-870
+407,773,750
+-104,29,83
+378,-903,-323
+-778,-728,485
+426,699,580
+-438,-605,-362
+-469,-447,-387
+509,732,623
+647,635,-688
+-868,-804,481
+614,-800,639
+595,780,-596
+
+--- scanner 4 ---
+727,592,562
+-293,-554,779
+441,611,-461
+-714,465,-776
+-743,427,-804
+-660,-479,-426
+832,-632,460
+927,-485,-438
+408,393,-506
+466,436,-512
+110,16,151
+-258,-428,682
+-393,719,612
+-211,-452,876
+808,-476,-593
+-575,615,604
+-485,667,467
+-680,325,-822
+-627,-443,-432
+872,-547,-609
+833,512,582
+807,604,487
+839,-516,451
+891,-625,532
+-652,-548,-490
+30,-46,-14";
+
+        #endregion
+
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual((79, 3621), Run(FileHelpers.ReadAllLinesFromString(_testData)));
+        }
+
+        [Test]
+        public void Solve()
+        {
+            FileHelpers.CheckInputs(_inputFilename);
+
+            Assert.AreEqual((372, 12241), Run(FileHelpers.EnumerateLines(_inputFilename)));
+        }
+
+        static readonly List<Func<(int X, int Y, int Z), (int X, int Y, int Z)>> Rotate = new()
+        {
+            p => p,
+            p => (p.X, -p.Z, p.Y),
+            p => (p.X, -p.Y, -p.Z),
+            p => (p.X, p.Z, -p.Y),
+            p => (-p.X, p.Y, -p.Z),
+            p => (-p.X, p.Z, p.Y),
+            p => (-p.X, -p.Y, p.Z),
+            p => (-p.X, -p.Z, -p.Y),
+
+            p => (p.Y, p.Z, p.X),
+            p => (p.Y, -p.X, p.Z),
+            p => (p.Y, -p.Z, -p.X),
+            p => (p.Y, p.X, -p.Z),
+            p => (-p.Y, p.Z, -p.X),
+            p => (-p.Y, p.X, p.Z),
+            p => (-p.Y, -p.Z, p.X),
+            p => (-p.Y, -p.X, -p.Z),
+
+            p => (p.Z, p.X, p.Y),
+            p => (p.Z, -p.Y, p.X),
+            p => (p.Z, -p.X, -p.Y),
+            p => (p.Z, p.Y, -p.X),
+            p => (-p.Z, p.X, -p.Y),
+            p => (-p.Z, p.Y, p.X),
+            p => (-p.Z, -p.X, p.Y),
+            p => (-p.Z, -p.Y, -p.X),
+        };
+
+        static (long,long) Run(IEnumerable<string> inputs)
+        {
+            Scanner? currentScanner = null;
+            var scanners = new List<Scanner>();
+
+            foreach (var line in inputs)
+            {
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                if (line.StartsWith("---"))
+                {
+                    currentScanner = new Scanner();
+                    scanners.Add(currentScanner);
+                }
+                else
+                {
+                    var tokens = line.Split(',').Select(int.Parse).ToArray();
+                    currentScanner!.AddPoint(tokens[0], tokens[1], tokens[2]);
+                }
+            }
+
+            scanners[0].Oriented = true;
+
+            while (scanners.Any(s => !s.Oriented))
+            {
+                int foundCount = 0;
+                var s0 = scanners[0];
+
+                for (int j = 1; j < scanners.Count; j++)
+                {
+                    var s1 = scanners[j];
+                    if (s1.Oriented) continue;
+
+                    foreach (var r in Rotate)
+                    {
+                        var s1Rotated = s1.Rotate(r);
+
+                        bool isMatch = false;
+
+                        for (int a = 0; a < s0.Points.Count; a++)
+                        {
+                            if (isMatch) break;
+                            var s0Translated = Translate(s0.Points, s0.Points[a]);
+                            for (int b = 0; b < s1Rotated.Count; b++)
+                            {
+                                var s1Translated = Translate(s1Rotated, s1Rotated[b]);
+                                int matchCount = 0;
+                                foreach (var p in s1Translated)
+                                {
+                                    if (s0Translated.Contains(p))
+                                    {
+                                        matchCount++;
+                                    }
+                                }
+
+                                if (matchCount >= 12)
+                                {
+                                    foundCount++;
+                                    Console.WriteLine(
+                                        $"Match {j} ({matchCount}) ({s1Rotated[b].X},{s1Rotated[b].Y},{s1Rotated[b].Z})");
+                                    isMatch = true;
+
+                                    var mergedPoints = new HashSet<(int x, int y, int z)>();
+                                    foreach (var p in s0.Points)
+                                    {
+                                        mergedPoints.Add((p.X, p.Y, p.Z));
+                                    }
+
+                                    scanners[j].Oriented = true;
+                                    scanners[j].Location = (s0.Points[a].X - s1Rotated[b].X,
+                                        s0.Points[a].Y - s1Rotated[b].Y,
+                                        s0.Points[a].Z - s1Rotated[b].Z);
+                                    var newPoints = scanners[j].Rotate(r);
+
+                                    foreach (var p in newPoints)
+                                    {
+                                        mergedPoints.Add((p.X + scanners[j].Location.X,
+                                            p.Y + scanners[j].Location.Y,
+                                            p.Z + scanners[j].Location.Z));
+                                    }
+
+                                    Console.WriteLine(
+                                        $"Scanner {j} @ ({scanners[j].Location.X},{scanners[j].Location.Y},{scanners[j].Location.Z})");
+
+                                    scanners[0].Points.Clear();
+                                    foreach (var p in mergedPoints)
+                                    {
+                                        scanners[0].AddPoint(p.x, p.y, p.z);
+                                    }
+
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (foundCount == 0)
+                {
+                    Assert.Fail("No matches");
+                }
+            }
+
+            long manhattan = long.MinValue;
+            foreach (var s0 in scanners)
+            {
+                foreach (var s1 in scanners)
+                {
+                    var d = Math.Abs(s0.Location.X - s1.Location.X) +
+                            Math.Abs(s0.Location.Y - s1.Location.Y) +
+                            Math.Abs(s0.Location.Z - s1.Location.Z);
+                    if (d > manhattan) manhattan = d;
+                }
+            }
+
+            return (scanners[0].Points.Count, manhattan);
+        }
+
+        static HashSet<(int x, int y, int z)> Translate(IList<(int X, int Y, int Z)> points, (int X, int Y, int Z) basePoint)
+        {
+            var outPoints = new HashSet<(int, int, int)>();
+            foreach (var point in points)
+            {
+                outPoints.Add((point.X - basePoint.X, point.Y - basePoint.Y, point.Z - basePoint.Z));
+            }
+
+            return outPoints;
+        }
+
+        class Scanner
+        {
+            public readonly List<(int X, int Y, int Z)> Points = new();
+            public bool Oriented { get; set; } = false;
+            public (int X, int Y, int Z) Location = (0, 0, 0);
+
+            public void AddPoint(int x, int y, int z)
+            {
+                Points.Add((x, y, z));
+            }
+
+            public List<(int X, int Y, int Z)> Rotate(Func<(int X, int Y, int Z), (int X, int Y, int Z)> rotateFunc)
+            {
+                return Points.Select(rotateFunc).ToList();
+            }
+        }
+    }
+}
